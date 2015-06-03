d30690a70e9b7a913e29d7befb0256ff3d693d82
manu09327 <manu09327@gmail.com>
2015-06-03 00:36:55 -0300

    Added main function

--- test.c
+++ test.c
@@ -1,7 +1,16 @@
-var b = require('bonescript');
-b.pinMode('P8_19', b.INPUT);
-b.digitalRead('P8_19', printStatus);
-function printStatus(x) {
-    console.log('x.value = ' + x.value);
-    console.log('x.err = ' + x.err);
+
+//Includes
+#include <stdio.h>
+#include <time.h>
+
+int main()
+{
+	var b = require('bonescript');
+    b.pinMode('P8_19', b.INPUT);
+    b.digitalRead('P8_19', printStatus);
+    function printStatus(x) {
+        console.log('x.value = ' + x.value);
+        console.log('x.err = ' + x.err);
+    }
+	return 0;
 }
